34d3ac2b4a385b5cd47c41af261745ff6803f3e8
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-03-15 01:36:32 -0300

    Create recipeBank.js

---
+++ src/data/recipeBank.js
@@ -0,0 +1,330 @@
+export const RECIPE_BANK = {
+
+cafe: [
+
+{
+id:"cafe_ovos_pao",
+title:"Ovos mexidos + pão",
+tags:["proteico","rápido"],
+steps:[
+"Quebre 2 a 3 ovos em uma tigela.",
+"Misture com sal e pimenta.",
+"Cozinhe na frigideira mexendo.",
+"Sirva com pão."
+]
+},
+
+{
+id:"cafe_iogurte_fruta",
+title:"Iogurte natural + fruta",
+tags:["leve","digestão"],
+steps:[
+"Coloque iogurte natural em um bowl.",
+"Corte banana ou morango.",
+"Misture com aveia."
+]
+},
+
+{
+id:"cafe_panq_aveia",
+title:"Panqueca de aveia",
+tags:["energia","pré treino"],
+steps:[
+"Misture 1 ovo, 2 colheres de aveia e 1 banana.",
+"Bata no liquidificador.",
+"Cozinhe em frigideira."
+]
+},
+
+{
+id:"cafe_tapioca_queijo",
+title:"Tapioca com queijo",
+tags:["brasileiro","energia"],
+steps:[
+"Aqueça a tapioca na frigideira.",
+"Adicione queijo.",
+"Dobre e sirva."
+]
+},
+
+{
+id:"cafe_sanduiche_frango",
+title:"Sanduíche de frango",
+tags:["proteico"],
+steps:[
+"Desfie frango cozido.",
+"Misture com requeijão.",
+"Monte no pão integral."
+]
+},
+
+{
+id:"cafe_vitamina_banana",
+title:"Vitamina de banana",
+tags:["rápido"],
+steps:[
+"Bata leite, banana e aveia.",
+"Sirva gelado."
+]
+},
+
+{
+id:"cafe_crepioca",
+title:"Crepioca",
+tags:["proteico"],
+steps:[
+"Misture 1 ovo com 2 colheres de tapioca.",
+"Cozinhe na frigideira.",
+"Recheie com queijo ou frango."
+]
+},
+
+{
+id:"cafe_iogurte_granola",
+title:"Iogurte + granola",
+tags:["rápido"],
+steps:[
+"Coloque iogurte natural.",
+"Adicione granola e fruta."
+]
+},
+
+{
+id:"cafe_pao_ovo_abacate",
+title:"Pão + ovo + abacate",
+tags:["energia"],
+steps:[
+"Toste pão.",
+"Adicione ovo e abacate amassado."
+]
+},
+
+{
+id:"cafe_banana_amendoim",
+title:"Banana + pasta de amendoim",
+tags:["pré treino"],
+steps:[
+"Corte banana.",
+"Adicione pasta de amendoim."
+]
+}
+
+],
+
+
+
+almoco:[
+
+{
+id:"alm_arroz_feijao_frango",
+title:"Arroz + feijão + frango",
+tags:["tradicional","proteico"],
+steps:[
+"Grelhe frango.",
+"Sirva com arroz e feijão.",
+"Adicione salada."
+]
+},
+
+{
+id:"alm_patinho_arroz",
+title:"Patinho moído + arroz",
+tags:["proteico"],
+steps:[
+"Refogue patinho com cebola.",
+"Sirva com arroz."
+]
+},
+
+{
+id:"alm_tilapia_arroz",
+title:"Tilápia grelhada + arroz",
+tags:["leve"],
+steps:[
+"Grelhe tilápia com limão.",
+"Sirva com arroz."
+]
+},
+
+{
+id:"alm_frango_batata",
+title:"Frango + batata",
+tags:["energia"],
+steps:[
+"Grelhe frango.",
+"Cozinhe batata.",
+"Sirva juntos."
+]
+},
+
+{
+id:"alm_omelete_arroz",
+title:"Omelete + arroz",
+tags:["rápido"],
+steps:[
+"Faça omelete com 3 ovos.",
+"Sirva com arroz."
+]
+},
+
+{
+id:"alm_frango_macarrao",
+title:"Frango + macarrão",
+tags:["energia"],
+steps:[
+"Cozinhe macarrão.",
+"Grelhe frango.",
+"Misture."
+]
+},
+
+{
+id:"alm_arroz_feijao_ovo",
+title:"Arroz + feijão + ovo",
+tags:["barato"],
+steps:[
+"Aqueça arroz e feijão.",
+"Faça ovos fritos."
+]
+},
+
+{
+id:"alm_quinoa_frango",
+title:"Quinoa + frango",
+tags:["fitness"],
+steps:[
+"Cozinhe quinoa.",
+"Grelhe frango.",
+"Misture com legumes."
+]
+},
+
+{
+id:"alm_sardinha_arroz",
+title:"Sardinha + arroz",
+tags:["barato"],
+steps:[
+"Abra sardinha.",
+"Sirva com arroz."
+]
+},
+
+{
+id:"alm_carne_batata",
+title:"Carne + batata",
+tags:["energia"],
+steps:[
+"Grelhe carne.",
+"Cozinhe batata."
+]
+}
+
+],
+
+
+
+janta:[
+
+{
+id:"jan_sopa_frango",
+title:"Sopa de frango",
+tags:["leve"],
+steps:[
+"Cozinhe frango com legumes.",
+"Sirva quente."
+]
+},
+
+{
+id:"jan_omelete_legumes",
+title:"Omelete com legumes",
+tags:["proteico"],
+steps:[
+"Bata ovos.",
+"Adicione legumes.",
+"Cozinhe."
+]
+},
+
+{
+id:"jan_frango_salada",
+title:"Frango + salada",
+tags:["cutting"],
+steps:[
+"Grelhe frango.",
+"Monte salada."
+]
+},
+
+{
+id:"jan_tilapia_legumes",
+title:"Peixe + legumes",
+tags:["leve"],
+steps:[
+"Grelhe peixe.",
+"Adicione legumes."
+]
+},
+
+{
+id:"jan_arroz_ovo",
+title:"Arroz + ovos",
+tags:["rápido"],
+steps:[
+"Aqueça arroz.",
+"Faça ovos mexidos."
+]
+},
+
+{
+id:"jan_sanduiche_frango",
+title:"Sanduíche de frango",
+tags:["rápido"],
+steps:[
+"Monte pão com frango."
+]
+},
+
+{
+id:"jan_tapioca_frango",
+title:"Tapioca + frango",
+tags:["proteico"],
+steps:[
+"Faça tapioca.",
+"Adicione frango."
+]
+},
+
+{
+id:"jan_macarrao_atum",
+title:"Macarrão + atum",
+tags:["energia"],
+steps:[
+"Cozinhe macarrão.",
+"Misture atum."
+]
+},
+
+{
+id:"jan_frango_brocolis",
+title:"Frango + brócolis",
+tags:["fitness"],
+steps:[
+"Grelhe frango.",
+"Cozinhe brócolis."
+]
+},
+
+{
+id:"jan_ovos_salada",
+title:"Ovos + salada",
+tags:["leve"],
+steps:[
+"Cozinhe ovos.",
+"Sirva com salada."
+]
+}
+
+]
+
+}
