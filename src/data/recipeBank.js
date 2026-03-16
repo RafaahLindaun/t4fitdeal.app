9cf7fb2c7c9387cf1833b8655b69608281c2c4e6
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-03-16 09:53:01 -0300

    Update recipeBank.js

--- src/data/recipeBank.js
+++ src/data/recipeBank.js
@@ -3,6 +3,383 @@ export const recipeBank = {
 cafe: [
 
 {
+id:"cafe31",
+title:"Panqueca de banana proteica",
+subtitle:"doce natural para começar o dia",
+minutes:8,
+calories:290,
+tags:["rápido","proteína"],
+ingredients:["banana","ovo","aveia","canela"],
+steps:[
+"amasse a banana",
+"misture com ovo e aveia",
+"faça panquecas na frigideira",
+"finalize com canela"
+]
+},
+
+{
+id:"cafe32",
+title:"Tapioca com queijo e ovo",
+subtitle:"energia simples",
+minutes:6,
+calories:320,
+tags:["rápido"],
+ingredients:["tapioca","ovo","queijo"],
+steps:[
+"prepare tapioca",
+"faça ovo mexido",
+"coloque dentro da tapioca"
+]
+},
+
+{
+id:"cafe33",
+title:"Iogurte com granola e frutas",
+subtitle:"leve e nutritivo",
+minutes:3,
+calories:250,
+tags:["leve"],
+ingredients:["iogurte natural","granola","morango"],
+steps:[
+"coloque iogurte em uma tigela",
+"adicione granola",
+"finalize com frutas"
+]
+},
+
+{
+id:"cafe34",
+title:"Omelete simples",
+subtitle:"proteína rápida",
+minutes:5,
+calories:240,
+tags:["proteína"],
+ingredients:["ovo","sal","pimenta"],
+steps:[
+"bata os ovos",
+"tempere",
+"cozinhe na frigideira"
+]
+},
+
+{
+id:"cafe35",
+title:"Vitamina de banana",
+subtitle:"energia líquida",
+minutes:3,
+calories:280,
+tags:["rápido"],
+ingredients:["banana","leite","aveia"],
+steps:[
+"coloque tudo no liquidificador",
+"bata até ficar homogêneo"
+]
+},
+
+{
+id:"cafe36",
+title:"Pão com pasta de amendoim",
+subtitle:"calórico para energia",
+minutes:2,
+calories:310,
+tags:["rápido"],
+ingredients:["pão","pasta de amendoim"],
+steps:[
+"passe pasta de amendoim no pão",
+"consuma"
+]
+},
+
+{
+id:"cafe37",
+title:"Aveia com leite",
+subtitle:"café da manhã clássico",
+minutes:5,
+calories:260,
+tags:["rotina"],
+ingredients:["aveia","leite","mel"],
+steps:[
+"misture aveia e leite",
+"adicione mel"
+]
+},
+
+{
+id:"cafe38",
+title:"Sanduíche de ovo",
+subtitle:"proteico e rápido",
+minutes:7,
+calories:330,
+tags:["proteína"],
+ingredients:["pão","ovo","queijo"],
+steps:[
+"faça ovo mexido",
+"monte no pão com queijo"
+]
+},
+
+{
+id:"cafe39",
+title:"Crepioca simples",
+subtitle:"rápido e leve",
+minutes:6,
+calories:290,
+tags:["rápido"],
+ingredients:["ovo","tapioca"],
+steps:[
+"misture ovo e tapioca",
+"cozinhe na frigideira"
+]
+},
+
+{
+id:"cafe40",
+title:"Frutas com iogurte",
+subtitle:"leve",
+minutes:2,
+calories:220,
+tags:["leve"],
+ingredients:["banana","morango","iogurte"],
+steps:[
+"misture frutas e iogurte"
+]
+},
+
+{
+id:"cafe41",
+title:"Panqueca de aveia",
+subtitle:"energia gradual",
+minutes:8,
+calories:300,
+tags:["rotina"],
+ingredients:["aveia","ovo","leite"],
+steps:[
+"misture ingredientes",
+"faça panquecas"
+]
+},
+
+{
+id:"cafe42",
+title:"Torrada com abacate",
+subtitle:"gordura saudável",
+minutes:5,
+calories:270,
+tags:["leve"],
+ingredients:["pão","abacate","sal"],
+steps:[
+"amasse abacate",
+"passe na torrada"
+]
+},
+
+{
+id:"cafe43",
+title:"Ovos mexidos",
+subtitle:"clássico proteico",
+minutes:4,
+calories:230,
+tags:["proteína"],
+ingredients:["ovo","sal"],
+steps:[
+"bata ovos",
+"faça mexido na frigideira"
+]
+},
+
+{
+id:"cafe44",
+title:"Smoothie de frutas",
+subtitle:"vitaminado",
+minutes:3,
+calories:240,
+tags:["leve"],
+ingredients:["banana","morango","leite"],
+steps:[
+"bata tudo no liquidificador"
+]
+},
+
+{
+id:"cafe45",
+title:"Cuscuz com ovo",
+subtitle:"tradicional",
+minutes:10,
+calories:340,
+tags:["rotina"],
+ingredients:["cuscuz","ovo"],
+steps:[
+"prepare cuscuz",
+"faça ovo mexido"
+]
+},
+
+{
+id:"cafe46",
+title:"Pão com queijo",
+subtitle:"simples",
+minutes:2,
+calories:280,
+tags:["rápido"],
+ingredients:["pão","queijo"],
+steps:["monte o sanduíche"]
+},
+
+{
+id:"cafe47",
+title:"Omelete com tomate",
+subtitle:"proteína leve",
+minutes:6,
+calories:250,
+tags:["proteína"],
+ingredients:["ovo","tomate"],
+steps:["bata ovos","adicione tomate","cozinhe"]
+},
+
+{
+id:"cafe48",
+title:"Granola com leite",
+subtitle:"energia",
+minutes:2,
+calories:260,
+tags:["rápido"],
+ingredients:["granola","leite"],
+steps:["misture"]
+},
+
+{
+id:"cafe49",
+title:"Panqueca simples",
+subtitle:"doce leve",
+minutes:8,
+calories:300,
+tags:["rotina"],
+ingredients:["farinha","ovo","leite"],
+steps:["misture","faça panquecas"]
+},
+
+{
+id:"cafe50",
+title:"Vitamina de morango",
+subtitle:"leve",
+minutes:3,
+calories:210,
+tags:["leve"],
+ingredients:["morango","leite"],
+steps:["bata tudo"]
+},
+
+{
+id:"cafe51",
+title:"Pão com ovo",
+subtitle:"proteico",
+minutes:5,
+calories:300,
+tags:["proteína"],
+ingredients:["pão","ovo"],
+steps:["faça ovo","coloque no pão"]
+},
+
+{
+id:"cafe52",
+title:"Tapioca doce",
+subtitle:"energia rápida",
+minutes:5,
+calories:260,
+tags:["rápido"],
+ingredients:["tapioca","banana"],
+steps:["prepare tapioca","coloque banana"]
+},
+
+{
+id:"cafe53",
+title:"Aveia com banana",
+subtitle:"saciedade",
+minutes:3,
+calories:280,
+tags:["rotina"],
+ingredients:["aveia","banana","leite"],
+steps:["misture"]
+},
+
+{
+id:"cafe54",
+title:"Iogurte com mel",
+subtitle:"leve",
+minutes:2,
+calories:200,
+tags:["leve"],
+ingredients:["iogurte","mel"],
+steps:["misture"]
+},
+
+{
+id:"cafe55",
+title:"Sanduíche natural",
+subtitle:"equilibrado",
+minutes:6,
+calories:320,
+tags:["proteína"],
+ingredients:["pão","frango","alface"],
+steps:["monte o sanduíche"]
+},
+
+{
+id:"cafe56",
+title:"Ovos cozidos",
+subtitle:"proteína pura",
+minutes:10,
+calories:180,
+tags:["proteína"],
+ingredients:["ovo"],
+steps:["cozinhe ovos"]
+},
+
+{
+id:"cafe57",
+title:"Tapioca com queijo",
+subtitle:"leve",
+minutes:5,
+calories:270,
+tags:["rápido"],
+ingredients:["tapioca","queijo"],
+steps:["prepare tapioca","adicione queijo"]
+},
+
+{
+id:"cafe58",
+title:"Vitamina de aveia",
+subtitle:"energia",
+minutes:3,
+calories:290,
+tags:["rotina"],
+ingredients:["leite","aveia","banana"],
+steps:["bata no liquidificador"]
+},
+
+{
+id:"cafe59",
+title:"Panqueca de banana",
+subtitle:"doce saudável",
+minutes:8,
+calories:280,
+tags:["leve"],
+ingredients:["banana","ovo"],
+steps:["misture","cozinhe"]
+},
+
+{
+id:"cafe60",
+title:"Pão integral com queijo",
+subtitle:"simples",
+minutes:2,
+calories:250,
+tags:["rápido"],
+ingredients:["pão integral","queijo"],
+steps:["monte"]
+}
+{
 id:"cafe_ovos_pao",
 title:"Ovos mexidos + pão",
 tags:["proteico","rápido"],
@@ -116,6 +493,116 @@ steps:[
 almoco:[
 
 {
+id:"almoco31",
+title:"Arroz, feijão e frango",
+subtitle:"clássico brasileiro",
+minutes:20,
+calories:520,
+tags:["rotina"],
+ingredients:["arroz","feijão","frango"],
+steps:["cozinhe arroz","aqueça feijão","grelhe frango"]
+},
+
+{
+id:"almoco32",
+title:"Frango grelhado com salada",
+subtitle:"leve e proteico",
+minutes:15,
+calories:420,
+tags:["proteína"],
+ingredients:["frango","alface","tomate"],
+steps:["grelhe frango","prepare salada"]
+},
+
+{
+id:"almoco33",
+title:"Macarrão com frango",
+subtitle:"energia",
+minutes:20,
+calories:550,
+tags:["rotina"],
+ingredients:["macarrão","frango","molho"],
+steps:["cozinhe macarrão","misture com frango"]
+},
+
+{
+id:"almoco34",
+title:"Arroz com carne",
+subtitle:"refeição forte",
+minutes:18,
+calories:560,
+tags:["proteína"],
+ingredients:["arroz","carne"],
+steps:["cozinhe arroz","grelhe carne"]
+},
+
+{
+id:"almoco35",
+title:"Omelete com arroz",
+subtitle:"proteico",
+minutes:12,
+calories:420,
+tags:["proteína"],
+ingredients:["ovo","arroz"],
+steps:["faça omelete","sirva com arroz"]
+},
+
+{
+id:"almoco36",
+title:"Frango com batata",
+subtitle:"ganho de energia",
+minutes:18,
+calories:500,
+tags:["rotina"],
+ingredients:["frango","batata"],
+steps:["asse frango","cozinhe batata"]
+},
+
+{
+id:"almoco37",
+title:"Salada com frango",
+subtitle:"leve",
+minutes:10,
+calories:350,
+tags:["leve"],
+ingredients:["frango","alface","tomate"],
+steps:["misture ingredientes"]
+},
+
+{
+id:"almoco38",
+title:"Macarrão simples",
+subtitle:"rápido",
+minutes:12,
+calories:430,
+tags:["rápido"],
+ingredients:["macarrão","molho"],
+steps:["cozinhe macarrão","adicione molho"]
+},
+
+{
+id:"almoco39",
+title:"Arroz com ovo",
+subtitle:"econômico",
+minutes:10,
+calories:420,
+tags:["rápido"],
+ingredients:["arroz","ovo"],
+steps:["faça ovo","sirva com arroz"]
+},
+
+{
+id:"almoco40",
+title:"Frango desfiado",
+subtitle:"proteína limpa",
+minutes:15,
+calories:380,
+tags:["proteína"],
+ingredients:["frango","temperos"],
+steps:["cozinhe frango","desfie"]
+},
+
+{
 id:"alm_arroz_feijao_frango",
 title:"Arroz + feijão + frango",
 tags:["tradicional","proteico"],
@@ -224,6 +711,116 @@ steps:[
 
 
 janta:[
+
+{
+id:"janta31",
+title:"Omelete com queijo",
+subtitle:"leve para noite",
+minutes:6,
+calories:260,
+tags:["proteína"],
+ingredients:["ovo","queijo"],
+steps:["bata ovos","adicione queijo","cozinhe"]
+},
+
+{
+id:"janta32",
+title:"Sanduíche natural",
+subtitle:"jantar rápido",
+minutes:5,
+calories:300,
+tags:["rápido"],
+ingredients:["pão","frango","alface"],
+steps:["monte sanduíche"]
+},
+
+{
+id:"janta33",
+title:"Salada com frango",
+subtitle:"leve",
+minutes:10,
+calories:320,
+tags:["leve"],
+ingredients:["frango","alface","tomate"],
+steps:["misture ingredientes"]
+},
+
+{
+id:"janta34",
+title:"Crepioca",
+subtitle:"proteica",
+minutes:7,
+calories:280,
+tags:["proteína"],
+ingredients:["ovo","tapioca"],
+steps:["misture e cozinhe"]
+},
+
+{
+id:"janta35",
+title:"Arroz com ovo",
+subtitle:"simples",
+minutes:10,
+calories:380,
+tags:["rotina"],
+ingredients:["arroz","ovo"],
+steps:["prepare arroz","faça ovo"]
+},
+
+{
+id:"janta36",
+title:"Frango grelhado",
+subtitle:"proteína pura",
+minutes:12,
+calories:300,
+tags:["proteína"],
+ingredients:["frango"],
+steps:["grelhe frango"]
+},
+
+{
+id:"janta37",
+title:"Ovos mexidos",
+subtitle:"rápido",
+minutes:4,
+calories:220,
+tags:["rápido"],
+ingredients:["ovo"],
+steps:["faça ovos mexidos"]
+},
+
+{
+id:"janta38",
+title:"Panqueca de aveia",
+subtitle:"leve",
+minutes:8,
+calories:260,
+tags:["leve"],
+ingredients:["aveia","ovo"],
+steps:["misture","cozinhe"]
+},
+
+{
+id:"janta39",
+title:"Salada simples",
+subtitle:"leve",
+minutes:5,
+calories:180,
+tags:["leve"],
+ingredients:["alface","tomate"],
+steps:["misture"]
+},
+
+{
+id:"janta40",
+title:"Tapioca com queijo",
+subtitle:"rápido",
+minutes:6,
+calories:290,
+tags:["rápido"],
+ingredients:["tapioca","queijo"],
+steps:["prepare tapioca"]
+},
 
 {
 id:"jan_sopa_frango",
