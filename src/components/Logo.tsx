dd7cdd2126c7da3cb64812dbbe0b647cb93e65ef
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-07-07 01:10:30 -0300

    Delete src directory

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export default function Logo({ compact = false }: { compact?: boolean }) {
-  return (
-    <div className={compact ? "accqua-logo compact" : "accqua-logo"}>
-      <span>ACCQUA</span>
-      <span>SPORTS</span>
-      {!compact && <small>ACADEMIA</small>}
-    </div>
-  );
-}
