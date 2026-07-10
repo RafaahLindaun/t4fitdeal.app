eec60aef88616b9e8ed92ad978974748617d9b95
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-07-10 00:41:59 -0300

    Delete src directory

--- src/components/AccquaLogo.tsx
+++
@@ -1,9 +0,0 @@
-export default function AccquaLogo({ animated = false }: { animated?: boolean }) {
-  return (
-    <div className={`accqua-logo ${animated ? "is-animated" : ""}`} aria-label="Accqua Sports Academia">
-      <span className="accqua-logo-line top" />
-      <img src="/accqua-logo-text.png" alt="Accqua Sports Academia" />
-      <span className="accqua-logo-line bottom" />
-    </div>
-  );
-}
