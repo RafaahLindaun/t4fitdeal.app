e8c9e1dd21740cef1ed8da0e267ea7814b9a782e
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-08-27 01:05:05 -0300

    Delete src/components directory

--- src/components/BottomNavTargetContext.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext, type RefObject } from "react";
-
-const BottomNavTreinoTargetContext = createContext<RefObject<HTMLSpanElement> | null>(null);
-
-export const BottomNavTreinoTargetProvider = BottomNavTreinoTargetContext.Provider;
-
-export function useBottomNavTreinoTarget() {
-  return useContext(BottomNavTreinoTargetContext);
-}
