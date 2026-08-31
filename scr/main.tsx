33e2cf722cb9c30b510341a6f16b2aa2e1a99aaf
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-08-31 18:26:14 -0300

    style: load Build 1.4.8.9 mobile Staff fixes

--- scr/main.tsx
+++ scr/main.tsx
@@ -11,6 +11,7 @@ import "./styles/build-1.4.7.css";
 import "./styles/build-1.4.8.css";
 import "./styles/build-1.4.8.1.css";
 import "./styles/build-1.4.8.2.css";
+import "./styles/build-1.4.8.9.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
