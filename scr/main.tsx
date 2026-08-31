5a95e9fbc4d88071e37175f865320d0783c8222a
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-08-31 11:34:17 -0300

    chore: load Build 1.4.8.1 global styles

--- scr/main.tsx
+++ scr/main.tsx
@@ -9,6 +9,7 @@ import "./styles/build-1.4.5-hardening.css";
 import "./styles/build-1.4.6.css";
 import "./styles/build-1.4.7.css";
 import "./styles/build-1.4.8.css";
+import "./styles/build-1.4.8.1.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
