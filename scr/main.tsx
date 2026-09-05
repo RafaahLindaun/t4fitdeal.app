87905491d3012f52ee8f97ba30569246bf9abe14
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-05 02:48:16 -0300

    build 1.6.5.7: mount profile milestone highlight

--- scr/main.tsx
+++ scr/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthProvider";
+import ProfileHighlights from "./components/ProfileHighlights";
 import { installInfrastructureToastGuard } from "./lib/staffErrors";
 import { installStaffDestructiveActionGuard } from "./lib/staffDestructiveActions";
 import "./index.css";
@@ -57,6 +58,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
       <BrowserRouter>
         <AuthProvider>
           <App />
+          <ProfileHighlights />
         </AuthProvider>
       </BrowserRouter>
     </QueryClientProvider>
