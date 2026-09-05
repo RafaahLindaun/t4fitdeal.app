e246c0a7ed7fd6f0f67e01a1d8af1428d07eb11b
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-05 02:46:28 -0300

    build 1.6.5.7: load partners tab styling

--- scr/main.tsx
+++ scr/main.tsx
@@ -30,6 +30,7 @@ import "./styles/login-1.6.5.3.css";
 import "./styles/window-motion-1.6.5.6.css";
 import "./styles/build-1.6.5.7-round2.css";
 import "./styles/ranking-1.6.5.7.css";
+import "./styles/profile-partners-1.6.5.7.css";
 
 installInfrastructureToastGuard();
 installStaffDestructiveActionGuard();
