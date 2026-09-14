c7866335e6444f00e2d7f065da944c012ff0007e
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-14 11:15:40 -0300

    revert: restore main imports to Build 1.7.0 baseline

--- scr/main.tsx
+++ scr/main.tsx
@@ -44,8 +44,6 @@ import "./styles/build-1.6.5.9-r3.css";
 import "./styles/retire-partners-1.6.5.9.css";
 import "./styles/build-1.7.0.css";
 import "./styles/build-1.7.0-capabilities.css";
-import "./styles/build-1.7.1-mobile-fixes.css";
-import "./styles/bottom-nav-mobile-final.css";
 
 installInfrastructureToastGuard();
 installStaffDestructiveActionGuard();
