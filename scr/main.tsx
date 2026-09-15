56f41f567155679a215b8e0c251d5b42d898e497
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-14 21:28:58 -0300

    style: load final Build 1.7.1 layer last

--- scr/main.tsx
+++ scr/main.tsx
@@ -54,6 +54,7 @@ import "./styles/calendar-classes-polish.css";
 import "./styles/cardio-assignment-polish.css";
 import "./styles/build-1.7.1-rounds-2-4.css";
 import "./styles/profile-tabs-1.7.1.css";
+import "./styles/build-1.7.1.css";
 
 installInfrastructureToastGuard();
 installStaffDestructiveActionGuard();
