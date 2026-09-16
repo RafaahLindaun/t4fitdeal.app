5c1f1bb5fc54cad4b02535f737c251b6d8a23370
RafaahLindaun <89591381+RafaahLindaun@users.noreply.github.com>
2026-09-16 03:14:50 -0300

    style(home): load rhythm polish

--- scr/main.tsx
+++ scr/main.tsx
@@ -58,6 +58,7 @@ import "./styles/profile-tabs-1.7.1.css";
 import "./styles/build-1.7.1.css";
 import "./styles/build-1.7.2.css";
 import "./styles/not-allowed-1.7.2.css";
+import "./styles/home-streak-1.7.2.css";
 
 installInfrastructureToastGuard();
 installStaffDestructiveActionGuard();
